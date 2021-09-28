b139c54cccdf281a9a8759653f233ed726e29512
Dafydd Thomas <dafydd@simpleonlinehealthcare.com>
2021-09-28 13:08:55 +0100

    Added fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            {process.env.NODE_ENV === 'production' && (
+                <Head>
+                    <script
+                        src="https://cdn.usefathom.com/script.js"
+                        data-site="EFTFIBDY"
+                        defer
+                    ></script>
+                </Head>
+            )}
+            <Component {...pageProps} />;
+        </div>
+    );
 }
 export default MyApp;
