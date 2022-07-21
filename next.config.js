5d98ba0e9068b8d9e3f6e87f7b9b85bc84b6a495
Dafydd Thomas <dafz@thomasweb.co>
2022-07-21 10:05:40 +0100

    Added rss feed to export

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
     reactStrictMode: true,
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            require('./lib/rss');
+        }
+
+        return config;
+    },
     async redirects() {
         return [
             {
